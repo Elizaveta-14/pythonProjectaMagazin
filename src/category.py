bd0be8ae5a06c5f3f86ccf409100dac67e0cc280
Elizaveta <you@example.com>
2025-02-10 19:12:35 +0300

    add proretry

--- src/category.py
+++ src/category.py
@@ -1,3 +1,6 @@
+from src.product import Product
+
+
 class Category:
     name: str
     description: str
@@ -8,6 +11,22 @@ class Category:
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
-        self.products = products
-        self.product_count += len(products)
+        self.__products = products
         Category.category_count += 1
+        Category.product_count += len(products)
+        print(Category.product_count)
+
+    def add_product(self, product: dict):
+        """Метод добавления нового продукта в список"""
+        if isinstance(product, Product):
+            self.__products.append(product)
+            Category.product_count += 1
+        else:
+            raise TypeError
+
+    @property
+    def get_product_list(self) -> str:
+        product_list = ""
+        for product in self.__products:
+            product_list += f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n"
+        return product_list
